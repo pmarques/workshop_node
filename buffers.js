38860f6a2a9c5add0d8f378d505b2bcc2975af83
Patrick F. Marques <patrick.marques@k2c.eu>
2014-04-13 11:35:05 +0100

    Buffers copy example

--- buffers.js
+++ buffers.js
@@ -1,12 +1,17 @@
 var buf1 = new Buffer(26);
 var buf2 = buf1.slice( 10, 20 );
 
+var buf3 = new Buffer( 10 );
+
 for (var i = 0 ; i < 26 ; i++) {
 	buf1[i] = i + 97; // 97 is ASCII a
 }
 
 console.log( buf1.toString() );
 console.log( buf2.toString() );
+console.log( buf3.toString() );
+
+buf1.copy( buf3 );
 
 for (var i = 0 ; i < 26 ; i++) {
 	buf2[i] = 33; // ASCII !
@@ -14,3 +19,4 @@ for (var i = 0 ; i < 26 ; i++) {
 
 console.log( buf1.toString() );
 console.log( buf2.toString() );
+console.log( buf3.toString() );
