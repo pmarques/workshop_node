be3b81e281d54abe2be7ba550772ebb279257e8c
Patrick F. Marques <patrick.marques@k2c.eu>
2014-04-13 11:34:49 +0100

    Buffers example

---
+++ buffers.js
@@ -0,0 +1,16 @@
+var buf1 = new Buffer(26);
+var buf2 = buf1.slice( 10, 20 );
+
+for (var i = 0 ; i < 26 ; i++) {
+	buf1[i] = i + 97; // 97 is ASCII a
+}
+
+console.log( buf1.toString() );
+console.log( buf2.toString() );
+
+for (var i = 0 ; i < 26 ; i++) {
+	buf2[i] = 33; // ASCII !
+}
+
+console.log( buf1.toString() );
+console.log( buf2.toString() );
